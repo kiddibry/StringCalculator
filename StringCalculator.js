9159871628514389030fc22e23e21243c3ce2f86
kiddibry <mrkrissibry@gmail.com>
2018-10-10 15:32:22 +0000

    1.

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,3 +1,13 @@
-function Add(string numbers){
+function Add(numbers){
 	if(numbers == "") return 0;
+	else if(!numbers.includes(",")){
+		return parseInt(numbers);
+	}
+	else{
+		var arr = numbers.split(",");
+		return parseInt(arr[0]) + parseInt(arr[1]);
+	}
+	return 0;
 }
+
+module.exports = Add;
